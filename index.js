27c9ba0418863e91b01c4c1411d2b58a3a4b653c
finnjordal <finn@jordal.dk>
2017-07-27 15:25:14 +0200

    IE11

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.corssupported= function () {
 exports.formatAdgangsadresse= function (mini, enlinje) {
 	let separator= (enlinje || typeof enlinje != 'undefined')?", ":"<br/>";
 	let supplerendebynavn= mini.supplerendebynavn?separator + mini.supplerendebynavn:"";
-	return `${mini.vejnavn} ${mini.husnr}${supplerendebynavn}${separator}${mini.postnr} ${mini.postnrnavn}`;	
+	return mini.vejnavn + " " + mini.husnr + supplerendebynavn + separator + mini.postnr + " " + mini.postnrnavn;	
 }
 
 exports.formatAdresse= function (mini, enlinje) {
@@ -13,11 +13,11 @@ exports.formatAdresse= function (mini, enlinje) {
 	let etagedør= (mini.etage?", "+mini.etage+".":"") + (mini.dør?" "+mini.dør:"");
 
 	let supplerendebynavn= mini.supplerendebynavn?separator + mini.supplerendebynavn:"";
-	return `${mini.vejnavn} ${mini.husnr}${etagedør}${supplerendebynavn}${separator}${mini.postnr} ${mini.postnrnavn}`;	
+	return mini.vejnavn + " " + mini.husnr + etagedør + supplerendebynavn + separator + mini.postnr + " " + mini.postnrnavn
 }
 
 exports.danUrl= function (path, query) {    
   var url = new URL(path);
-  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+  Object.keys(query).forEach(function(key) {url.searchParams.append(key, query[key])});
   return url;
 }
